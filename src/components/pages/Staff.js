0dad170cf738700e046a32c94e18aba07ad0ce8b
Saigal Amatya <saigal.amatya@gmail.com>
2018-07-02 10:45:14 +0545

    authentication implemented

--- src/components/pages/Staff.js
+++ src/components/pages/Staff.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export default () => {
-  return (
-    <div>
-      <h1>Staff</h1>
-    </div>
-  )
+class Staff extends Component {
+
+  state = {
+    currentUserName: '',
+    currentUserEmail: ''
+  }
+
+  componentDidMount() {
+    const idToken = JSON.parse(localStorage.getItem('okta-token-storage'));
+    this.setState({
+      currentUserEmail: idToken.idToken.claims.email,
+      currentUserName: idToken.idToken.claims.name
+    })
+  }
+
+  render() {
+    console.log(this.state);
+    const { currentUserEmail, currentUserName } = this.state;
+    return (
+      <div>
+        <h1>Welcome { currentUserName }</h1>
+        <p>You have reached the authorize staff area of the portal.</p>
+      </div>
+    )
+  }
 }
+
+export default Staff;
